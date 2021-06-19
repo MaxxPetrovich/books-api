95f181165027c248b30edd585162a8a71e3f6c96
MP <pepper@inbox.ru>
2021-06-19 20:25:31 +0300

    Initil config

---
+++ src/test/java/ru/mpetrov/booksapi/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mpetrov.booksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
